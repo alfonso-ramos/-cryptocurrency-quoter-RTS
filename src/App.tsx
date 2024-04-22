bdc631423d4db293adb2f394c858d86edb13a94c
Alfonso Ramos <alfonsoramos.1803@gmail.com>
2024-04-22 14:03:32 -0700

    [ADD] Empezando proyecto de cotizador de criptomonedas

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './index.css'
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <h1 className='app-title'>
+          Cotizador de <span> criptomonedas</span>
+        </h1>
+        <div className="content">
+          
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
